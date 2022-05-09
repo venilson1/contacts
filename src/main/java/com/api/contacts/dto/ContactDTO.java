080b9c7478cc3ee62c0020aabbc692c22b95800c
venilson1 <venilson2009@hotmail.com>
2022-05-09 19:12:56 -0300

    update entites

--- src/main/java/com/api/contacts/dto/ContactDTO.java
+++ src/main/java/com/api/contacts/dto/ContactDTO.java
@@ -10,62 +10,62 @@ import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 public class ContactDTO implements Serializable {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private final Long id;
+	private final Long id;
 
-    @NotEmpty(message = "Campo obrigatório")
-    @Size(min = 3, message = "Nome minímo de 3 carácter")
-    private final String name;
-    @NotEmpty(message = "Campo obrigatório")
-    private final String lastName;
-    @Pattern(regexp = "(^\\d{3}\\x2E\\d{3}\\x2E\\d{3}\\x2D\\d{2}$)")
-    private final String cpf;
-    @Email(message = "favor entrar com email válido")
-    private final String email;
-    @NotEmpty(message = "Campo obrigatório")
-    private final String telephone;
+	@NotEmpty(message = "Campo obrigatório")
+	@Size(min = 3, message = "Nome minímo de 3 carácter")
+	private final String name;
+	@NotEmpty(message = "Campo obrigatório")
+	private final String lastName;
+	@Pattern(regexp = "(^\\d{3}\\x2E\\d{3}\\x2E\\d{3}\\x2D\\d{2}$)")
+	private final String cpf;
+	@Email(message = "favor entrar com email válido")
+	private final String email;
+	@NotEmpty(message = "Campo obrigatório")
+	private final String telephone;
 
-    public ContactDTO(Long id, String name, String lastName, String cpf, String email, String telephone) {
-        this.id = id;
-        this.name = name;
-        this.lastName = lastName;
-        this.cpf = cpf;
-        this.email = email;
-        this.telephone = telephone;
-    }
+	public ContactDTO(Long id, String name, String lastName, String cpf, String email, String telephone) {
+		this.id = id;
+		this.name = name;
+		this.lastName = lastName;
+		this.cpf = cpf;
+		this.email = email;
+		this.telephone = telephone;
+	}
 
-    public ContactDTO(@NotNull Contact entity){
-        this.id = entity.getId();
-        this.name = entity.getName();
-        this.lastName = entity.getName();
-        this.cpf = entity.getCpf();
-        this.email = entity.getEmail();
-        this.telephone = entity.getTelephone();
-    }
+	public ContactDTO(@NotNull Contact entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+		this.lastName = entity.getName();
+		this.cpf = entity.getCpf();
+		this.email = entity.getEmail();
+		this.telephone = entity.getTelephone();
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public String getCpf() {
-        return cpf;
-    }
+	public String getCpf() {
+		return cpf;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String getTelephone() {
-        return telephone;
-    }
+	public String getTelephone() {
+		return telephone;
+	}
 
 }
--- src/main/java/com/api/contacts/entites/Contact.java
+++ src/main/java/com/api/contacts/entites/Contact.java
@@ -4,85 +4,83 @@ import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
-@Table(name = "contacts")
+@Table(name = "tb_contact")
 public class Contact implements Serializable {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private String name;
+	private String name;
 
-    private String lastName;
+	private String lastName;
 
-    @Column(unique = true)
-    private String cpf;
+	@Column(unique = true)
+	private String cpf;
 
-    @Column(unique = true)
-    private String email;
+	@Column(unique = true)
+	private String email;
 
+	private String telephone;
 
-    private String telephone;
+	public Contact() {
+	}
 
-    public Contact() {
-    }
+	public Contact(Long id, String name, String lastName, String cpf, String email, String telephone) {
+		this.id = id;
+		this.name = name;
+		this.lastName = lastName;
+		this.cpf = cpf;
+		this.email = email;
+		this.telephone = telephone;
+	}
 
-    public Contact(Long id, String name, String lastName, String cpf, String email, String telephone) {
-        this.id = id;
-        this.name = name;
-        this.lastName = lastName;
-        this.cpf = cpf;
-        this.email = email;
-        this.telephone = telephone;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public String getCpf() {
+		return cpf;
+	}
 
-    public String getCpf() {
-        return cpf;
-    }
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public String getTelephone() {
+		return telephone;
+	}
 
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
 }
