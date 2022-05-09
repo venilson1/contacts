d1ad96d352046c850825ad9bd33081466c16d6b0
venilson1 <venilson2009@hotmail.com>
2022-05-09 17:25:54 -0300

    method GET contact

--- src/main/java/com/api/contacts/resources/ContactResource.java
+++ src/main/java/com/api/contacts/resources/ContactResource.java
@@ -1,4 +1,26 @@
 package com.api.contacts.resources;
 
+import com.api.contacts.dto.ContactDTO;
+import com.api.contacts.services.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/contatos")
 public class ContactResource {
+
+    @Autowired
+    private ContactService service;
+
+    @GetMapping
+    public ResponseEntity<List<ContactDTO>> findAll(){
+        List<ContactDTO> contacts = service.findAll();
+        return ResponseEntity.ok().body(contacts);
+    }
+
 }
--- src/main/java/com/api/contacts/services/ContactService.java
+++ src/main/java/com/api/contacts/services/ContactService.java
@@ -1,4 +1,27 @@
 package com.api.contacts.services;
 
+import com.api.contacts.dto.ContactDTO;
+import com.api.contacts.entites.Contact;
+import com.api.contacts.repositories.ContactRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class ContactService {
+
+    @Autowired
+	private ContactRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<ContactDTO> findAll(){
+        List<Contact> contacts = repository.findAll();
+        List<ContactDTO> contactsDTO = contacts.stream().map(el -> new ContactDTO(el)).collect(Collectors.toList());
+        return contactsDTO;
+    }
+
 }
