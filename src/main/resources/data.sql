67dbe6b5f9d803406b1a4fd9d88a8e742332fffa
venilson1 <venilson2009@hotmail.com>
2022-05-09 19:13:31 -0300

    seeding database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+/*
+INSERT INTO tb_contact (name, last_name, cpf, email, telephone) VALUES ('Bob', 'Brown', '56118812307', 'bob@gmail.com', '011956568978');
+*/
+
+INSERT INTO tb_contact (name, last_name, cpf, email, telephone) VALUES ('Bob', 'Brown', '23558896580', 'bob@gmail.com', '011956293003');
