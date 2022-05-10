8d540139bbc256b3bbf835c47017ff8be5732f3a
venilson1 <venilson2009@hotmail.com>
2022-05-10 09:00:58 -0300

    method DELETE contact

--- src/main/java/com/api/contacts/services/ContactService.java
+++ src/main/java/com/api/contacts/services/ContactService.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -62,5 +63,13 @@ public class ContactService {
 			throw new ResourceNotFoundException("Id Not Found " + id);
 		}
 	}
+	
+	  public void delete(Long id) {
+		    try {
+		      repository.deleteById(id);
+		    } catch (EmptyResultDataAccessException e) {
+		      throw new ResourceNotFoundException("Id Not Found " + id);
+		    } 
+		  }
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,4 @@
-/*
-INSERT INTO tb_contact (name, last_name, cpf, email, telephone) VALUES ('Bob', 'Brown', '56118812307', 'bob@gmail.com', '011956568978');
-*/
-
-INSERT INTO tb_contact (name, last_name, cpf, email, telephone) VALUES ('Bob', 'Brown', '235.588.965-80', 'bob@gmail.com', '011956293003');
+INSERT INTO tb_contact (name, last_name, cpf, email, telephone) VALUES ('Bob', 'Brown', '235.588.965-56', 'bob@gmail.com', '011959058945');
+INSERT INTO tb_contact (name, last_name, cpf, email, telephone) VALUES ('Mary', 'Crawler', '123.698.695-79', 'mary@gmail.com', '011969582514');
+INSERT INTO tb_contact (name, last_name, cpf, email, telephone) VALUES ('Charlie', 'Jeen', '456.741.125-09', 'charlie@gmail.com', '011978561265');
+INSERT INTO tb_contact (name, last_name, cpf, email, telephone) VALUES ('Rock', 'Balboa', '789.398.987-00', 'rock@gmail.com', '011974123698');
